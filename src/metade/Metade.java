57f1f2d7e736a720cd6220a0a3c1e992301f979d
henriqueprogramador <haugusto455@gmail.com>
2021-01-15 14:03:33 -0300

    mostra metade de um número

---
+++ src/metade/Metade.java
@@ -0,0 +1,78 @@
+
+package metade;
+
+import java.util.Scanner;
+
+
+public class Metade {
+
+    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        float n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
+        float metade;
+        
+        System.out.println("Informe 10 numeros pausadamente. "
+                + " Digite o 1º numero: ");
+        n1 = entrada.nextFloat();
+        metade = n1/2;
+        System.out.println(metade);  
+       
+        
+         System.out.println("2º numero: ");
+        n2 = entrada.nextFloat();
+        metade = n2/2;
+        System.out.println(metade); 
+        
+        
+         System.out.println("3º numero: ");
+        n3 = entrada.nextFloat();
+        metade = n3/2;
+        System.out.println(metade);
+     
+        
+         System.out.println("4º numero: ");
+        n4 = entrada.nextFloat();
+        metade = n4/2;
+        System.out.println(metade); 
+     
+        
+         System.out.println("5º numero: ");
+        n5 = entrada.nextFloat();
+        metade = n5/2;
+        System.out.println(metade);
+        
+        
+         System.out.println("6º numero: ");
+        n6 = entrada.nextFloat();
+        metade = n6/2;
+        System.out.println(metade);
+      
+        
+         System.out.println("7º numero: ");
+        n7 = entrada.nextFloat();
+        metade = n7/2;
+        System.out.println(metade);
+        
+        
+         System.out.println("8º numero: ");
+        n8 = entrada.nextFloat();
+        metade = n8/2;
+        System.out.println(metade);  
+      
+        
+         System.out.println("9º numero: ");
+        n9 = entrada.nextFloat();
+        metade = n9/2;
+        System.out.println(metade);  
+      
+        
+         System.out.println("10º numero: ");
+        n10 = entrada.nextFloat();
+        metade = n10/2;
+        System.out.println(metade);
+     
+    }
+    
+}
